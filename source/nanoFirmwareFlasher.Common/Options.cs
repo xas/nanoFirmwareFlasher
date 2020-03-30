ae4da267de7944ef2fa389fc63930bef0b31ec29
xas <xas@users.noreply.github.com>
2020-03-30 21:48:14 +0200

    Options classes implementation
    * renamed `stm32` platform to `stm32dfu`
    * Implementation of ListOptions
    * Implementation of BackupOptions
    * Implementation of EraseOptions
    * Implementation of WriteOptions
    * Implementation of CommandLine unit test for parsing

--- source/nanoFirmwareFlasher.Common/Options.cs
+++ source/nanoFirmwareFlasher.Common/Options.cs
@@ -1,5 +1,5 @@
 ﻿using CommandLine;
-using System;
+using System.Collections.Generic;
 
 namespace nanoFirmwareFlasher.Common
 {
@@ -11,24 +11,24 @@ namespace nanoFirmwareFlasher.Common
      *  You can 'write' to a device
      *  
      *  If you 'list' :
-     *  - You choose a 'target' platform. 'target' is always required : 'stm32', 'stm32jtag', 'esp32', 'ti'
+     *  - You choose a 'target' platform. 'target' is always required : 'stm32dfu', 'stm32jtag', 'esp32', 'ti'
      *  - Q/A : Does 'esp32' and 'ti' could 'list' something ?
      *  
      *  If you 'backup' :
-     *  - You choose a 'target' platform. 'target' is always required : 'stm32', 'stm32jtag', 'esp32', 'ti'
-     *      - For 'stm32' and 'stm32jtag', you choose a 'device'. If missing, take the first one. Or all ?
+     *  - You choose a 'target' platform. 'target' is always required : 'stm32dfu', 'stm32jtag', 'esp32', 'ti'
+     *      - For 'stm32dfu' and 'stm32jtag', you choose a 'device'. If missing, take the first one. Or all ?
      *      - For 'esp32', you need to define 'port', 'baud', and 'freq' => 'port' required. The rest ?
      *      - For 'TI' ?
      *  
      *  If you 'erase' :
-     *  - You choose a 'target' platform. 'target' is always required : 'stm32', 'stm32jtag', 'esp32', 'ti'
-     *      - For 'stm32' and 'stm32jtag', you choose a 'device'. DO NOT erase if not provided
+     *  - You choose a 'target' platform. 'target' is always required : 'stm32dfu', 'stm32jtag', 'esp32', 'ti'
+     *      - For 'stm32dfu' and 'stm32jtag', you choose a 'device'. DO NOT erase if not provided
      *      - For 'esp32', you need to define 'port', 'baud', and 'freq' => 'port' required. The rest ?
      *      - For 'TI' ?
      *  
      *  If you 'write' :
-     *  - You choose a 'target' platform. 'target' is always required : 'stm32', 'stm32jtag', 'esp32', 'ti'
-     *      - For 'stm32' and 'stm32jtag', you choose a 'device'. Required.
+     *  - You choose a 'target' platform. 'target' is always required : 'stm32dfu', 'stm32jtag', 'esp32', 'ti'
+     *      - For 'stm32dfu' and 'stm32jtag', you choose a 'device'. Required.
      *      - For 'esp32', you need to define 'port', 'baud', and 'freq' => at least 'port' required
      *      - For 'TI' ?
      *  - Set 'image' to write from a local file
@@ -40,34 +40,132 @@ namespace nanoFirmwareFlasher.Common
      */
     public enum Platform
     {
-        stm32,
+        stm32dfu,
         stm32jtag,
         esp32,
         ti
     }
 
-    public abstract class CommonOptions
+    public abstract class ACommonOptions
     {
+        [Option("platform", Required = true, HelpText = "Target platform. Acceptable values are: esp32, stm32dfu, stm32jtag, ti.")]
+        public Platform Platform { get; set; }
+
+        #region STM32/JTAG specific
+        [Option("device", HelpText = "ID of the DFU/JTAG STM32 device to update. If not specified the first connected device will be used.")]
+        public string DeviceId { get; set; }
+        #endregion
+
+        #region ESP32 specific
+        [Option(
+            "port",
+            Required = false,
+            Default = null,
+            HelpText = "Serial port where device is connected to.")]
+        public string SerialPort { get; set; }
+
+        [Option(
+            "baud",
+            Required = false,
+            Default = 921600,
+            HelpText = "Baud rate to use for the serial port.")]
+        public int BaudRate { get; set; }
+
+        [Option(
+            "flashfreq",
+            Required = false,
+            Default = 40,
+            HelpText = "Flash frequency to use [MHz].")]
+        public int FlashFrequency { get; set; }
+
+        [Option(
+            "flashmode",
+            Required = false,
+            Default = "dio",
+            HelpText = "Flash mode to use.")]
+        public string FlashMode { get; set; }
+
+        #endregion
+
+        /// <summary>
+        /// Allowed values:
+        /// q[uiet]
+        /// m[inimal]
+        /// n[ormal]
+        /// d[etailed]
+        /// diag[nostic]
+        /// </summary>
+        [Option(
+            'v',
+            "verbosity",
+            Required = false,
+            Default = "n",
+            HelpText = "Sets the verbosity level of the command. Allowed values are q[uiet], m[inimal], n[ormal], d[etailed], and diag[nostic]. Not supported in every command; see specific command page to determine if this option is available.")]
+        public string Verbosity { get; set; }
+
+        public virtual bool PostValidate() { return true; }
     }
 
-    [Verb("list")]
-    public class ListOptions : CommonOptions
+    [Verb("list", HelpText = "List all device available for a specific platform")]
+    public class ListOptions : ACommonOptions
     {
 
     }
 
-    [Verb("backup")]
-    public class BackupOptions : CommonOptions
+    [Verb("backup", HelpText = "Backup the firmware for a specific platform")]
+    public class BackupOptions : ACommonOptions
     {
+        [Option("savepath", Required = true, HelpText = "Full path where to the backup file to create.")]
+        public string PathToSave { get; set; }
     }
 
-    [Verb("erase")]
-    public class EraseOptions : CommonOptions
+    [Verb("erase", HelpText = "Erase the firmware for a specific platform")]
+    public class EraseOptions : ACommonOptions
     {
+        public override bool PostValidate()
+        {
+            switch (Platform)
+            {
+                case Platform.stm32dfu:
+                case Platform.stm32jtag:
+                    if (string.IsNullOrEmpty(DeviceId))
+                    {
+                        return false;
+                    }
+                    break;
+            }
+            return base.PostValidate();
+        }
     }
 
-    [Verb("write")]
-    public class WriteOptions : CommonOptions
+    [Verb("write", HelpText = "Write a firmware for a specific platform. Can write a local file or download it from community repositories")]
+    public class WriteOptions : ACommonOptions
     {
+        [Option("image", SetName = "LocalFile", Group = "File", Separator = ';')]
+        public IEnumerable<string> Images { get; set; }
+
+        [Option("community", SetName = "RemoteFile", Group = "File")]
+        public string CommunityImage { get; set; }
+
+        [Option("nightly", HelpText = "Download a nightly build of the firmware. If omitted, the stable version will be used")]
+        public bool BetaVersion { get; set; }
+
+        [Option("address")]
+        public string Address { get; set; }
+
+        public override bool PostValidate()
+        {
+            switch (Platform)
+            {
+                case Platform.stm32dfu:
+                case Platform.stm32jtag:
+                    if (string.IsNullOrEmpty(DeviceId))
+                    {
+                        return false;
+                    }
+                    break;
+            }
+            return base.PostValidate();
+        }
     }
 }
--- source/nanoFirmwareFlasher.Tests/ParserTest.cs
+++ source/nanoFirmwareFlasher.Tests/ParserTest.cs
@@ -9,9 +9,50 @@ namespace nanoFirmwareFlasher.Tests
     public class ParserTest
     {
         [Test]
-        public void CommandLineTest()
+        [TestCaseSource(nameof(GenerateCommandLineArgs))]
+        public void CommandLineTest(IEnumerable<string> args, bool expectedParsing)
         {
-            Assert.That(true, Is.True);
+            Parser.Default.ParseArguments<ListOptions, BackupOptions, WriteOptions, EraseOptions>(args)
+                .WithParsed<ListOptions>(options => { if (!expectedParsing) Assert.Fail("Fully parsed but expecting not", args); })
+                .WithParsed<BackupOptions>(options => { if (!expectedParsing) Assert.Fail("Fully parsed but expecting not", args); })
+                .WithParsed<WriteOptions>(options => { if (!expectedParsing) Assert.Fail("Fully parsed but expecting not", args); })
+                .WithParsed<EraseOptions>(options => { if (!expectedParsing) Assert.Fail("Fully parsed but expecting not", args); })
+                .WithNotParsed(errs => { if (expectedParsing) Assert.Fail("Not parsed but expecting it", args); });
+        }
+
+        private static IEnumerable<object[]> GenerateCommandLineArgs()
+        {
+            // Action : 'list'
+            yield return new object[] { "list --platform stm32dfu".Split(' '), true };
+            yield return new object[] { "list --platform stm32jtag".Split(' '), true };
+            yield return new object[] { "list --platform esp32".Split(' '), true };
+            yield return new object[] { "list --platform ti".Split(' '), true };
+            yield return new object[] { "list --platform foo".Split(' '), false };
+            yield return new object[] { "list foo".Split(' '), false };
+            yield return new object[] { "list --erase --target deviceid".Split(' '), false };
+
+            // Action : 'write'
+            yield return new object[] { "write --platform stm32dfu --image filepath".Split(' '), true };
+            yield return new object[] { "write --platform stm32dfu --image filepath1;filepath2".Split(' '), true };
+            yield return new object[] { "write --platform stm32dfu --image filepath1;filepath2;".Split(' '), true };
+            yield return new object[] { "write --platform stm32dfu --community filepath1".Split(' '), true };
+            yield return new object[] { "write --platform stm32dfu".Split(' '), false };
+            yield return new object[] { "write --platform foo".Split(' '), false };
+            yield return new object[] { "write foo".Split(' '), false };
+            yield return new object[] { "write --erase --target deviceid".Split(' '), false };
+
+            // Action : 'backup'
+            yield return new object[] { "backup --platform stm32dfu --savepath filepath".Split(' '), true };
+            yield return new object[] { "backup --platform stm32dfu".Split(' '), false };
+            yield return new object[] { "backup --platform foo".Split(' '), false };
+            yield return new object[] { "backup foo".Split(' '), false };
+            yield return new object[] { "backup --erase --target deviceid".Split(' '), false };
+
+            // Action : 'erase'
+            yield return new object[] { "erase --platform stm32dfu".Split(' '), true };
+            yield return new object[] { "erase --platform foo".Split(' '), false };
+            yield return new object[] { "erase foo".Split(' '), false };
+            yield return new object[] { "erase --erase --target deviceid".Split(' '), false };
         }
     }
 }
